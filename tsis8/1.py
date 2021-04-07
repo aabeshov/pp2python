3afec55139ca2f690c389e0c76d842a323985d71
Alen <a_abeshov@kbtu.kz>
2021-04-07 19:22:44 +0600

    Обновление(Были улучшены модели автомобиля)

--- tsis8/1.py
+++ tsis8/1.py
@@ -110,7 +110,7 @@ class Enemy(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("player1.png")
+        self.image = pygame.image.load("player12.png")
         self.image = pygame.transform.scale(self.image,(40,90))
         self.surf = pygame.Surface((50, 100))
         self.rect = self.surf.get_rect(center = (400
